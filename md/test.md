104b86222a39e625ebb058a7e69854944a70f4d3
russvanfossen <russ.vanfossen@gmail.com>
2024-05-14 18:14:13 -0400

    first commit

--- md/test.md
+++ md/test.md
@@ -1,2 +1,61 @@
-Here is some markdown text. 
-# Markdown!!
+# H1
+## H2
+### H3
+**bold text**
+
+*italicized text*
+> blockquote
+
+1. First item
+2. Second item
+3. Third item
+
+- First item
+- Second item
+- Third item
+
+`code`
+
+---
+
+[title](https://www.example.com)
+
+![alt text](image.jpg)
+
+First Header  | Second Header
+------------- | -------------
+Content Cell  | Content Cell
+Content Cell  | Content Cell
+
+
+```
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+```
+
+
+Here's a sentence with a footnote. [^1]
+
+[^1]: This is the footnote.
+
+### My Great Heading {#custom-id}
+
+term
+: definition
+
+~~The world is flat.~~
+
+-  [x] Write the press release
+-  [ ] Update the website
+-  [ ] Contact the media
+
+That is so funny! :joy:
+
+I need to highlight these ==very important words==.
+
+H~2~O
+
+X^2^
--- routes.py
+++ routes.py
@@ -1,7 +1,61 @@
-from flask import render_template, request, url_for, redirect, abort, flash
+from flask import render_template, redirect
 import os
 import markdown
 from time import ctime
+from pymdownx import emoji
+
+extensions = [
+    'pymdownx.magiclink',
+    'pymdownx.betterem',
+    'pymdownx.tilde',
+    'pymdownx.emoji',
+    'pymdownx.tasklist',
+    'pymdownx.superfences',
+    'pymdownx.saneheaders',
+    'pymdownx.tasklist',
+    'pymdownx.mark',
+    'pymdownx.smartsymbols',
+    'pymdownx.caret',
+    'pymdownx.extra',
+    'sane_lists',
+    'nl2br',
+
+]
+
+extension_configs = {
+    'pymdownx.extra':{
+        'markdown.extensions.tables':{
+            'use_align_attribute': True
+        }
+    },
+    "pymdownx.magiclink": {
+        "repo_url_shortener": True,
+        "repo_url_shorthand": True,
+        "provider": "github",
+        "user": "facelessuser",
+        "repo": "pymdown-extensions"
+    },
+    "pymdownx.tilde": {
+        "subscript": True
+    },
+    "pymdownx.emoji": {
+        "emoji_index": emoji.gemoji,
+        "emoji_generator": emoji.to_png,
+        "alt": "short",
+        "options": {
+            "attributes": {
+                "align": "absmiddle",
+                "height": "20px",
+                "width": "20px"
+            },
+            "image_path": "https://github.githubassets.com/images/icons/emoji/unicode/",
+            "non_standard_image_path": "https://github.githubassets.com/images/icons/emoji/"
+        }
+    }
+}
+
+
+
 
 def register_routes(app):
 
@@ -21,7 +75,7 @@ def register_routes(app):
         if fold not in folder_contents:
             return redirect('/')
         MDContent = []
-        with open(os.path.join(app.root_path, 'md', fold)) as mdfile:
-            MDContent.append([markdown.markdown(mdfile.read(), extensions=['fenced_code', 'codehilite']), fold,
+        with open(os.path.join(app.root_path, 'md', fold)) as mdfile: 
+            MDContent.append([markdown.markdown(mdfile.read(), extensions=extensions, extension_configs=extension_configs), fold,
                               ctime(os.path.getctime(os.path.join(app.root_path, 'md', fold)))])
         return render_template('context_md.html', MDContent=MDContent)
